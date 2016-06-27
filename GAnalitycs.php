7560064b44b6ffdd4a2ba87530c401a54cd77bea
gerka <gerka23@gmail.com>
2016-06-27 11:39:14 +0300

    Nice start

---
+++ GAnalitycs.php
@@ -0,0 +1,53 @@
+<script type="text/javascript">
+	
+</script>
+<?php
+
+/**
+* Автор: Верещак Герман 
+* E-mail: gerka23@gmail.com
+* GitHub: gerka23
+*/
+class GAnalytic
+{
+	
+	function __construct(argument)
+	{
+		echo "
+	
+		";
+	}
+
+	public function ImpressionData($value='')
+	{
+		# code...
+	}
+
+	public function ProductData($value='')
+	{
+		# code...
+	}
+
+	public function PromotionData($value='')
+	{
+		# code...
+	}
+
+	public function ActionData($value='')
+	{
+		# code...
+	}
+
+	public function PPActions($value='')
+	{
+		# code...
+	}
+
+	public function RefundsData($value='')
+	{
+		# code...
+	}
+
+}
+
+?>
