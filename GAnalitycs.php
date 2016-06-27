51e4d766991636a3a67014961082cb1202f7686e
gerka <gerka23@gmail.com>
2016-06-27 12:02:50 +0300

    Добавлен пример Impression Data
    Можно сказать это пример того как будет работать скрипт. Вы должны
    вызвать функцию с патерном JS, который создаст массив типа Каталог:
    товар1, товар2, и тд.

--- GAnalitycs.php
+++ GAnalitycs.php
@@ -14,38 +14,77 @@ class GAnalytic
 	function __construct(argument)
 	{
 		echo "
-	
+			
 		";
 	}
 
-	public function ImpressionData($value='')
+	public function ImpressionData($value='.product_catalog > li')
 	{
-		# code...
+		$result = "$(".$value.").each(function(){
+		i = i+1;
+		product_catalog[i] = {
+		            // объект - impressionFieldObject
+		  'id': ID,                   // ID товара (string). Обязательный!
+		  'name': name, // Название товара (string). Обязательный!
+		  'category': category,   // Категория товара (string).
+		  'brand': brand,                // Бренд (string).
+		  'variant': variant,               // Варианты товара (например, цвет) (string).
+		  'list': list,         // Список, которому принадлежит товар (string).
+		  'position': i                 // Номер в списке товаров (number).  
+		}
 	}
+ 		ga('ec:addImpression', product_catalog[i]);
+		ga('ec:setAction', 'detail');
+			}"
+			return $result;
+			
 
 	public function ProductData($value='')
 	{
-		# code...
+		ga('ec:addProduct', {               // Объект - productFieldObject.
+			  'id': 'P12345',                   // ID товара (string). Обязательный!
+			  'name': 'Android Warhol T-Shirt', // Название товара (string).
+			  'category': 'Apparel',            // Категория товара (string).
+			  'brand': 'Google',                // Product brand (string).
+			  'variant': 'Black',               // Product variant (string).
+			  'position': 1                    // Product position (number).
+			});
 	}
 
 	public function PromotionData($value='')
 	{
-		# code...
+		ga('ec:addPromo', {               // Объект - promoFieldObject.
+		  'id': 'PROMO_1234',             // Идентификатор акции (string). Обязательный!
+		  'name': 'Summer Sale',          // Название акции (string). Обязательный!
+		  'creative': 'summer_banner2',   // Источник перехода (например, баннер), (string).
+		  'position': 'banner_slot1'      // Тип (например, позиция в слайдере), (string).
+		});
 	}
 
 	public function ActionData($value='')
 	{
-		# code...
+		ga('ec:setAction', 'purchase', {          // Объект -  actionFieldObject.
+		  'id': 'T12345',                         // ID товара (string). Обязательный!
+		  'affiliation': 'Google Store - Online', // Магазин, в котором произошла сделка (string).
+		  'revenue': '37.39',                     // Общая стоимость.
+		  'tax': '2.85',                          // Налог.
+		  'shipping': '5.34',                     // Доставка.
+		  'coupon': 'SUMMER2013'                  // Купон (промо-код) (string).
+		});
 	}
 
 	public function PPActions($value='')
 	{
-		# code...
+		ga('ec:setAction', 'click', {       // действие.
+			  'list': 'Search Results'          // Список товаров (string).
+			});
 	}
 
 	public function RefundsData($value='')
 	{
-		# code...
+		ga('ec:setAction', 'refund', {
+			  'id': 'T12345'    // ID транзакции.
+			});
 	}
 
 }
